76b02e36a2263f2bab2e741e28a294f27568575c
Nicole Adams <nicoleadams.boston@gmail.com>
2020-04-09 10:04:30 -0400

    add print room name

--- src/adv.py
+++ src/adv.py
@@ -51,8 +51,8 @@ print(my_Player)
 
 # Write a loop that:
 #
-for i in Player:
-    print(i.room)
+for i in room:
+    print(i)
 
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
